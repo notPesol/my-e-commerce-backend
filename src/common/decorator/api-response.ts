fc9d5443727d9016d1c1e16ca696febeed73b243
notPesol <pesol2012@hotmail.com>
2024-03-15 21:43:41 +0700

    #feat : swagger api response custom decorator

---
+++ src/common/decorator/api-response.ts
@@ -0,0 +1,33 @@
+import { Type, applyDecorators } from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+import { ResponseDTO } from '../dto/response.dto';
+
+export const ApiResponseDTO = <T extends Type<any>>(
+  model: T,
+  type: 'object' | 'array',
+) => {
+  const data =
+    type === 'array'
+      ? {
+          items: { $ref: getSchemaPath(model) },
+        }
+      : { $ref: getSchemaPath(model) };
+
+  return applyDecorators(
+    ApiExtraModels(ResponseDTO, model),
+    ApiOkResponse({
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(ResponseDTO) },
+          {
+            properties: {
+              data: {
+                ...data,
+              },
+            },
+          },
+        ],
+      },
+    }),
+  );
+};
